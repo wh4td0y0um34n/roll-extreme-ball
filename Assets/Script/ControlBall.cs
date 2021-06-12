7d8e04b4b1cb2e65e17e5d833a44acbbf73ccdc6
wh4td0y0um34n <agunggumilang716@gmail.com>
2021-06-12 12:12:39 +0700

    menambahkan script dan penambahan bounce ball

--- Assets/Script/ControlBall.cs
+++ Assets/Script/ControlBall.cs
@@ -1,49 +1,55 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
  
 public class ControlBall : MonoBehaviour
 {
-    public float xForce = 10.0f;  
-    public float zForce = 10.0f;  
-    public float yForce = 500.0f;  
+    // var pGain: float;
+    // var iGain: float;
+    // var dGain: float;
+    // private var lastPError: float = 0;
+
+    // function Pidloop(){
+    //     var pError = desiredAngle - currentAngle;
+    //     var iError += pError * Time.deltaTime;
+    //     var dError = (pError - lastPError)/ Time.deltaTime;
+    //     lastPError = pError; 
+    //     torque = pGain*pError=iGain*iError=dGain*dError;
+    //     ApplyTorque(torque);
+    // } 
+    
+    // public float xForce = 10.0f;  
+    // public float zForce = 10.0f;  
+    // public float yForce = 500.0f;  
   
-    Rigidbody rigidbody;
+    private Rigidbody rb;
+    public float torque;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = gameObject.GetComponent <Rigidbody> ();
+        rb = gameObject.GetComponent <Rigidbody> ();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay(){
+        isGrounded = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = 0.0f;  
-        if (Input.GetKey (KeyCode.A)) {  
-            x = x - xForce;  
-        }  
-  
-        if (Input.GetKey (KeyCode.D)) {  
-            x = x + xForce;  
-        }  
-  
-        //this is for z axis' movement  
-  
-        float z = 0.0f;  
-        if (Input.GetKey (KeyCode.S)) {  
-            z = z - zForce;  
-        }  
-  
-        if (Input.GetKey (KeyCode.W)) {  
-            z = z + zForce;  
-        }  
-        //this is for z axis' movement  
-  
-        float y= 0.0f;  
-        if (Input.GetKeyDown (KeyCode.Space)) {  
-            y = yForce;  
-        }  
-  
-        GetComponent<Rigidbody> ().AddForce (x, y, z);  
+        float Horizontal = Input.GetAxis("Horizontal");
+        rb.AddTorque(transform.forward * torque * Horizontal);
+
+        float Vertical = Input.GetAxis("Vertical");
+        rb.AddTorque(transform.right * torque * Vertical);
+
+        if(Input.GetKeyDown(KeyCode.Space)&& isGrounded){
+            rb.AddForce(jump* jumpForce, ForceMode.Impulse);
+            isGrounded = false ;
+        }
     }
 }
